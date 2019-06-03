78bbd8b8b07426768261d0cbc868541cd6c9668b
vagrant <asucarlos>
2019-06-03 23:43:30 +0000

    authorization added

--- download_avatars.js
+++ download_avatars.js
@@ -1,8 +1,19 @@
 var request = require('request');
+var secret = require('./secrests')
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var options = {
+  request('https://api.github.com/repos/jquery/jquery/contributors' + repoOwner + '/' + repoName + '/contributors',
+  headers: {
+    'user-Agent': 'request'
+    'Authorization':
+  }
+};
+
+request(options, function(err, res, body){
+    cb(err, body)
+  })
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
