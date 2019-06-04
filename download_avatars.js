6b6917b3463002a2332ce8cb258394b418f2aeda
vagrant <asucarlos>
2019-06-04 01:23:43 +0000

    finalized the project(indent, comment)

--- download_avatars.js
+++ download_avatars.js
@@ -4,55 +4,50 @@ var fs = require('fs');
 var repoOwner = process.argv[2];
 var repoName = process.argv[3];
 
-
+//higher function
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
     headers: {
       'user-Agent': 'request',
       Authorization: 'token ' + secrets.GITHUB_TOKEN
-
     }
   }
 
+//pasing from JSON to Object and calling the calling back function
 request(options, function(err, res, body){
     var data = JSON.parse(body)
-    // console.log(data)
-    // for(contributor of data){
-      cb(err, data)
-      // console.log(contributor.avatar_url)
-    // }
+    cb(err, data)
   })
 }
 
+//downloads image from url and stores in filepath
 function downloadImageByURL(url, filepath) {
   request
-  .get(url)
-  .on('error', function(err){
-    console.log('error occured')
-    throw err;
-  })
-  .on('response', function(response){
-    console.log('Downloading image...')
-    console.log(response.statusMessage, response.headers['content-type'])
-  })
-  .on('end', function(){
-    console.log('Download completed.')
-  })
-  .pipe(fs.createWriteStream(filepath));
+    .get(url)
+    .on('error', function(err){
+      console.log('error occured')
+      throw err;
+    })
+    .on('response', function(response){
+      console.log('Downloading image...')
+      console.log(response.statusMessage, response.headers['content-type'])
+    })
+    .on('end', function(){
+      console.log('Download completed.')
+    })
+    .pipe(fs.createWriteStream(filepath));
 }
 
+//calling the funtion and Callback function
 getRepoContributors(repoOwner, repoName, function(err, result) {
   if(!err && repoOwner && repoName){
     for(contributor of result){
-  // console.log(contributor.avatars);
       downloadImageByURL(contributor.avatar_url, "./avatars/"+ contributor.login + ".png" )
     }
-    }else {
+  } else {
       console.log("Errors:", err);
-    }
-  // console.log("Result:", result);
+  }
   });
 
-  // downloadImageByURL(result.avatar_url, "./avatars/"+ result.login + ".jpg" )
 
