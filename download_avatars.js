dc401cee70a8d5b5b1e70c23a723e061e106d4be
vagrant <asucarlos>
2019-06-04 00:26:31 +0000

    avatar_url collected

--- download_avatars.js
+++ download_avatars.js
@@ -1,25 +1,26 @@
 var request = require('request');
-var secret = require('./secrets')
+var secrets = require('./secrets')
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
-  url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
-  headers: {
-    'user-Agent': 'request'
-    'Authorization':
-  }
+    url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {
+      'user-Agent': 'request',
+      Authorization: 'Token ' + secrets.GITHUB_TOKEN
 
-};
+    }
+  }
 
 request(options, function(err, res, body){
     var data = JSON.parse(body)
     // console.log(data)
-    for(i in data){
-      console.log(data[i].avator_url)
+    for(contributor of data){
+      console.log(contributor.avatar_url)
     }
     cb(err, data)
   })
 }
+
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
