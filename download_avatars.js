6db509f47d41c8434979c0b0b0a9ce37717aa776
vagrant <asucarlos>
2019-06-04 00:43:47 +0000

    download function added

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,13 @@
 var request = require('request');
-var secrets = require('./secrets')
+var secrets = require('./secrets');
+var fs = require('fs');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
     headers: {
       'user-Agent': 'request',
-      Authorization: 'Token ' + secrets.GITHUB_TOKEN
+      Authorization: 'token ' + secrets.GITHUB_TOKEN
 
     }
   }
@@ -21,8 +22,27 @@ request(options, function(err, res, body){
   })
 }
 
+function downloadImageByURL(url, filepath) {
+  request
+  .get(url)
+  .on('error', function(err){
+    console.log('error occured')
+    throw err;
+  })
+  .on('response', function(response){
+    console.log('Downloading image...')
+    console.log(response.statusMessage, response.headers['content-type'])
+  })
+  .on('end', function(){
+    console.log('Download completed.');
+  })
+  .pipe(fs.createWriteStream('./avatar_img.jpg'))
+}
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  // console.log("Result:", result);
 });
+
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
